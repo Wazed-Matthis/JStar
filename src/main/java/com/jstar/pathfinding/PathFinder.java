ac2d42847abfa56f85119ce8b04fa4dca29f26b7
Wazed <wazed.wzd@gmail.com>
2020-07-10 00:46:33 +0200

    Buggy unclean push (lots oif errors)

--- src/main/java/com/jstar/pathfinding/PathFinder.java
+++ src/main/java/com/jstar/pathfinding/PathFinder.java
@@ -1,18 +1,48 @@
 package com.jstar.pathfinding;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 
 public class PathFinder {
 
     private final ArrayList<PathNode> openSet = new ArrayList<PathNode>();
+    private final ArrayList<PathNode> closedSet = new ArrayList<PathNode>();
 
     private final HashMap<PathNode, Float> gScoreMap = new HashMap<PathNode, Float>();
     private PathNode currentPoint;
 
-    public Path calculatePath(PathNode start, PathNode goal, float hCost){
+    public Path calculatePath(PathNode start, PathNode goal, float hCost) {
         openSet.add(start);
+
+        while (!openSet.isEmpty()) {
+            currentPoint = openSet.stream().min(Comparator.comparingDouble(PathNode::getwCost)).orElse(null);
+
+            if (currentPoint.equals(goal)) {
+
+            }
+
+            closedSet.add(currentPoint);
+            //TODO: renaming (I am bad at naming things)
+            for (PathNode currentNode : currentPoint.getNeighbouringNodes()) {
+                if (closedSet.contains(currentNode))
+                    continue;
+                //TODO: implement Heuristic G function
+                final float tentG = /*g(currentPoint,currentNode) + */ currentPoint.getDistanceSq(currentNode);
+
+                if (openSet.contains(currentNode) && tentG >= currentNode.getgCost())
+                    continue;
+
+                currentNode.setPreviousNode(currentPoint);
+                currentNode.setgCost(tentG);
+                //TODO: finish
+            }
+        }
         return null;
+    }
+
+    private Path backTrace(PathNode current) {
+
     }
 
 }
--- src/main/java/com/jstar/pathfinding/PathNode.java
+++ src/main/java/com/jstar/pathfinding/PathNode.java
@@ -1,17 +1,36 @@
 package com.jstar.pathfinding;
 
+import java.util.Objects;
+
 public class PathNode {
 
-    private PathNode[] neighbouringNodes;
+    private PathNode[] neighbouringNodes = new PathNode[27];
 
     private PathNode previousNode;
 
-    private final int x, y, z;
-    private float gCost, wCost, hCost;
+    private int x;
+    private int y;
+    private int z;
+    private float gCost;
+    private float wCost;
+    private float hCost;
     private boolean valid;
+    private boolean visited;
     private final int hash;
 
     public PathNode(int x, int y, int z) {
+        /**
+         * TODO: This is not working like it should, please fix
+         * TODO: improve performance or find another solution to add neighbours
+         */
+        for(int i = -1; i < 1; i++){
+            for(int j = -1; j < 1; j++){
+                for(int k = -1; k < 1; k++){
+                    neighbouringNodes[i*j*k] = add(i,j,k);
+                }
+            }
+        }
+
         this.hash = hashCode();
         this.x = x;
         this.y = y;
@@ -25,6 +44,13 @@ public class PathNode {
         return dX * dX + dY * dY + dZ * dZ;
     }
 
+    public PathNode add(int x, int y, int z){
+        final int aX = this.x += x;
+        final int aY = this.y += y;
+        final int aZ = this.z += z;
+        return new PathNode(aX,aY,aZ);
+    }
+
     public int getX() {
         return x;
     }
@@ -35,6 +61,14 @@ public class PathNode {
 
     public int getZ() {
         return z;
+    }
+
+    public void setPreviousNode(PathNode previousNode) {
+        this.previousNode = previousNode;
+    }
+
+    public PathNode getPreviousNode() {
+        return previousNode;
     }
 
     public float getgCost() {
@@ -59,5 +93,32 @@ public class PathNode {
 
     public boolean isValid() {
         return this.valid;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PathNode pathNode = (PathNode) o;
+        return x == pathNode.x &&
+                y == pathNode.y &&
+                z == pathNode.z &&
+                valid == pathNode.valid;
+    }
+
+    public void setgCost(float gCost) {
+        this.gCost = gCost;
     }
 }
